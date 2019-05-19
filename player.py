0aeb1663899ac8494c38fb2fb11a0912fccf13e9
Matan Cohen <34000779+comatan96@users.noreply.github.com>
2019-05-19 21:09:48 +0300

    Check jump functionality

--- player.py
+++ player.py
@@ -13,22 +13,31 @@ class Bird:
         self.location = Point(WIDTH//3, HEIGHT//2)
         self._gravity = GRAVITY
         self._velocity = 0
+        # TODO: check jump functionality
         self.jump = False
         self.hit = False
 
     def update_bird(self):
         location = self.location
 
+        # change velocity according to gravity in order to make the fast fall
         self._velocity = self._velocity + (self._gravity)
+
+        # change y location of the bird based on velocity
         self.location = self.location._replace(y=location.y + self._velocity)
 
+        # if jumped - update velocity
         if (pyxel.btnp(pyxel.KEY_SPACE) or pyxel.btnp(pyxel.KEY_UP))and not self.hit:
             self._velocity = -8
 
     def draw_bird(self):
         if self.location.y < HEIGHT - 20:
+
+            # save locations
             bird_x = self.location.x
             bird_y = self.location.y
+
+            # in order to animate, change v according to frames
             bird_v = ((pyxel.frame_count) // 4) % 3 * 16
             pyxel.blt(x=bird_x, y=bird_y, img=0, u=0,
                       v=bird_v, w=17, h=13, colkey=0)
